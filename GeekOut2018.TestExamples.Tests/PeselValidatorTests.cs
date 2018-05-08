2ea89069b627a633670647d5f759f24d198b0870
Tomasz Ściepłek <tomasz.scieplek@enova.pl>
2018-05-08 11:36:33 +0200

    Simple pesel validator with tests

---
+++ GeekOut2018.TestExamples.Tests/PeselValidatorTests.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+
+namespace GeekOut2018.TestExamples.Tests
+{
+    [TestFixture]
+    public class PeselValidatorTests
+    {
+        [Test]
+        public void Validate_PeselTooShort_ReturnsFasle()
+        {
+            //Arrange
+            var validator = new PeselValidator();
+
+            //Act
+            var result = validator.Validate("111");
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Validate_PeselTooLong_ReturnsFasle()
+        {
+            //Arrange
+            var validator = new PeselValidator();
+
+            //Act
+            var result = validator.Validate("11111111111111");
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Validate_MaleValidPesel_ReturnsTrue()
+        {
+            //Arrange
+            var validator = new PeselValidator();
+
+            //Act
+            var result = validator.Validate("22011970815");
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Validate_FemaleValidPesel_ReturnsTrue()
+        {
+            //Arrange
+            var validator = new PeselValidator();
+
+            //Act
+            var result = validator.Validate("68011951207");
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Validate_PeselWithWrongSum_ReturnsFalse()
+        {
+            //Arrange
+            var validator = new PeselValidator();
+
+            //Act
+            var result = validator.Validate("68011951208");
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ GeekOut2018.TestExamples/PeselValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace GeekOut2018.TestExamples
+{
+    public class PeselValidator
+    {
+        public bool Validate(string pesel)
+        {
+            var result = false;
+            try
+            {
+                if (pesel.Length == 11)
+                {
+                    result = CheckSum(pesel).Equals(pesel[10].ToString());
+                }
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        private static readonly int[] Multipliers = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+
+        private static string CheckSum(string pesel)
+        {
+            var sum = Multipliers.Select((t, i) => t * int.Parse(pesel[i].ToString())).Sum();
+            var remainder = sum % 10;
+            return remainder == 0 ? remainder.ToString() : (10 - remainder).ToString();
+        }
+    }
+}
