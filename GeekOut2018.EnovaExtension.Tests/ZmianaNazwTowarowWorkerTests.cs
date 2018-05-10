410fc0885102751afeff3b572743998cb7ce125c
Tomasz Ściepłek <tomasz.scieplek@enova.pl>
2018-05-10 13:28:24 +0200

    Testing with Soneta.Test library

---
+++ GeekOut2018.EnovaExtension.Tests/ZmianaNazwTowarowWorkerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using Soneta.Test.Poligon;
+using Soneta.Towary;
+
+namespace GeekOut2018.EnovaExtension.Tests
+{
+    [TestFixture]
+    public class ZmianaNazwTowarowWorkerTests : DbTransactionTestBase
+    {
+        private Towar Bikini => Get<Towar>(new Guid("65336878-70cf-4e64-bd72-b742cd26a657"));
+        private Towar Buty45 => Get<Towar>(new Guid("61e50151-9f26-4de3-85dc-5d0983f56956"));
+        private Towar Montaz => Get<Towar>(new Guid("0f8a8597-e2d1-40a6-a8e5-cc1045228660"));
+
+        [Test]
+        public void ZmianaNazwTowarowWorker_CanAddSuffix_SuffixIsAdded()
+        {
+            // Arrange
+            var worker = new ZmianaNazwTowarowWorker(Context)
+            {
+                Towary = new[] { Bikini },
+                Params = new ZmianaNazwTowarowParams(Context)
+                {
+                    DodajPostfiks = "xxx",
+                    TypTowaru = TypTowaru.Towar
+                }
+            };
+
+            // Act
+            worker.ZmianaNazw();
+
+            // Assert
+            Assert.AreEqual("Bikini - Strój kąpielowy damskixxx", Bikini.Nazwa);
+        }
+
+    }
+}
